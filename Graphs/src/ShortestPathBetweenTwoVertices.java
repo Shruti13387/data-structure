bd60f9352908e0841bfd964b1cbe27b5ae763715
Shruti13387 <thakurshruti29@gmail.com>
2021-10-27 00:26:59 +0530

    Shortest path between two vertex

--- Graphs/src/ShortestPathBetweenTwoVertices.java
+++ Graphs/src/ShortestPathBetweenTwoVertices.java
@@ -1,9 +1,18 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class PathExistBetweenTwoVertices {
+public class ShortestPathBetweenTwoVertices {
 
     public static void main(String[] args) {
+        Graph g=new Graph(5);
+        g.addEdge(0,1);
+        g.addEdge(0,2);
+        g.addEdge(1,3);
+        g.addEdge(3,4);
+        g.addEdge(1,4);
+        System.out.println("Shortest Path: "+ findMin(g, 0, 4));
+
+
         Graph g1 = new Graph(9);
         g1.addEdge(0,2);
         g1.addEdge(0,5);
@@ -17,8 +26,9 @@ public class PathExistBetweenTwoVertices {
         g1.addEdge(6,4);
         g1.addEdge(7,8);
         g1.printGraph();
-        System.out.println("Path exists: " + checkPath(g1, 0, 7));
+        System.out.println("Shortest Path: " + findMin(g1, 0, 7));
         System.out.println();
+
         Graph g2 = new Graph(4);
         g2.addEdge(0,1);
         g2.addEdge(1,2);
@@ -26,38 +36,39 @@ public class PathExistBetweenTwoVertices {
         g2.addEdge(2,3);
 
         g2.printGraph();
-        System.out.println("Path exists: " + checkPath(g2, 3, 0));
+        System.out.println("Shortest Path: " + findMin(g2, 3, 0));
     }
 
-    private static boolean checkPath(Graph graph, int source, int destination){
+    private static int findMin(Graph graph, int source, int destination){
         Queue<Integer> queue = new LinkedList<>();
         boolean[] visited = new boolean[graph.vertices];
+        int[] distance = new int[graph.vertices];
         if(graph.vertices < 1){
-            return false;
-        }
-        if(source==destination){
-            return true;
+            return 0;
         }
         for(int vertex =0; vertex < graph.vertices; vertex++) {
             if(vertex==source) {
                 queue.add(vertex);
                 visited[vertex] = true;
+
                 while (!queue.isEmpty() && graph.adjacencyList[queue.peek()] != null) {
-                    LinkedList<Integer> adjacent = graph.adjacencyList[queue.poll()];
+                    int removeNode = queue.poll();
+                    LinkedList<Integer> adjacent = graph.adjacencyList[removeNode];
                     if (!adjacent.isEmpty()) {
                         for (int i = 0; i < adjacent.size(); i++) {
-                            if(adjacent.get(i)==destination) {
-                                return true;
-                            }
                             if (!visited[adjacent.get(i)]) {
                                 queue.add(adjacent.get(i));
+                                distance[adjacent.get(i)] = distance[removeNode]+1;
                                 visited[adjacent.get(i)] = true;
+                            }
+                            if(adjacent.get(i)==destination) {
+                                return distance[adjacent.get(i)];
                             }
                         }
                     }
                 }
             }
         }
-        return false;
+        return -1;
     }
 }
